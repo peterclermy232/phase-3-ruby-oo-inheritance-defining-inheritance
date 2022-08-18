8480ce5848692711d4e3a8f38bff7f69d1c704e3
peterclermy <peterclermy232@gmail.com>
2022-08-18 19:28:29 +0300

    commit tree 12637ebd66acf5a9dae5dcc02e947a0c93cf9e57

--- .history/lib/car_20220818192453.rb
+++ .history/lib/car_20220818192453.rb
@@ -1,6 +1,2 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-class Car < Vehicle
-    def go
-      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    end
-  end
+require_relative './vehicle'
--- .history/lib/car_20220818192805.rb
+++ .history/lib/car_20220818192805.rb
@@ -1,6 +1,1 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-class Car < Vehicle
-    def go
-      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    end
-  end
